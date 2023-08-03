2eb7c9887207233ec4b95f5a9eeb8cb7f6b90360
Connor-York <connor.york000@gmail.com>
2023-08-03 13:25:22 +0100

    updated arduino file, ends with !

--- src/getDatatest/getDatatest.ino
+++ src/getDatatest/getDatatest.ino
@@ -34,10 +34,15 @@ void setup()
 void loop()
 {
   if (sensor.available()) {
-    Serial.print("Acc\t"); Serial.print(sensor.Acc.X); Serial.print("\t"); Serial.print(sensor.Acc.Y); Serial.print("\t"); Serial.println(sensor.Acc.Z); //acceleration information of X,Y,Z
-    Serial.print("Gyro\t"); Serial.print(sensor.Gyro.X); Serial.print("\t"); Serial.print(sensor.Gyro.Y); Serial.print("\t"); Serial.println(sensor.Gyro.Z); //angular velocity information of X,Y,Z
-    Serial.print("Angle\t"); Serial.print(sensor.Angle.X); Serial.print("\t"); Serial.print(sensor.Angle.Y); Serial.print("\t"); Serial.println(sensor.Angle.Z); //angle information of X, Y, Z 
-    Serial.println(" ");
+    // Serial.print("Acc\t"); Serial.print(sensor.Acc.X); Serial.print("\t"); Serial.print(sensor.Acc.Y); Serial.print("\t"); Serial.println(sensor.Acc.Z); //acceleration information of X,Y,Z
+    // Serial.print("Gyro\t"); Serial.print(sensor.Gyro.X); Serial.print("\t"); Serial.print(sensor.Gyro.Y); Serial.print("\t"); Serial.println(sensor.Gyro.Z); //angular velocity information of X,Y,Z
+    // Serial.print("Angle\t"); Serial.print(sensor.Angle.X); Serial.print("\t"); Serial.print(sensor.Angle.Y); Serial.print("\t"); Serial.println(sensor.Angle.Z); //angle information of X, Y, Z 
+    // Serial.println(" ");
+
+    Serial.print(sensor.Acc.X); Serial.print(","); Serial.print(sensor.Acc.Y); Serial.print(","); Serial.print(sensor.Acc.Z); Serial.print(",");
+    Serial.print(sensor.Gyro.X); Serial.print(","); Serial.print(sensor.Gyro.Y); Serial.print(","); Serial.print(sensor.Gyro.Z); Serial.print(",");
+    Serial.print(sensor.Angle.X); Serial.print(","); Serial.print(sensor.Angle.Y); Serial.print(","); Serial.print(sensor.Angle.Z); 
+    Serial.println("//");
   }
 }
 
